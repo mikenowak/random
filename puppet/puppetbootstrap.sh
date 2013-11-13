8f875514a03bf6eedabbad5cd488f6d720be0be6
Michal Nowak <michal@casanowak.com>
2013-11-13 21:15:55 +0000

    Added new siteadm script to manage hiera - will do for the moment, but needs to be rewriten soon

--- puppet/puppetbootstrap.sh
+++ puppet/puppetbootstrap.sh
@@ -70,18 +70,22 @@ if [ ! -d /etc/puppet/modules ]; then mkdir /etc/puppet/modules; fi
 if [ ! -d /etc/puppet/hieradata ]; then mkdir /etc/puppet/hieradata; fi
 
 # Install some puppet modules
+cd /etc/puppet/modules
 puppet module install puppetlabs-firewall
 puppet module install puppetlabs-stdlib
 puppet module install puppetlabs-concat
-puppet module install puppetlabs-apache
+#puppet module install puppetlabs-apache
+git clone https://github.com/puppetlabs/puppetlabs-apache.git apache
 puppet module install puppetlabs-mysql
 
 # And some configs
 wget -O /etc/puppet/manifests/site.pp https://raw.github.com/mikenowak/random/master/puppet/site.pp
 wget -O /etc/puppet/hiera.yaml https://raw.github.com/mikenowak/random/master/puppet/hiera.yaml
+wget -O /usr/local/bin/siteadm https://raw.github.com/mikenowak/random/master/puppet/siteadm && chmod +x /usr/local/bin/siteadm
 wget -O /etc/puppet/hieradata/$HOSTNAME.yaml https://raw.github.com/mikenowak/random/master/puppet/$HOSTNAME.yaml
 
 puppet apply --show_diff --verbose /etc/puppet/manifests/site.pp
 
-# reboot for a good measure
-echo 'now you should init 6'
+rm $0
+
+init 6
