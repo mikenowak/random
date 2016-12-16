612e6dc39ad93ad355eddc4b6f1db903c9e2bdd2
Mike Nowak <mikenowak@users.noreply.github.com>
2016-12-16 00:45:00 +0000

    adding  a script to reload perf counter on win2k3 serv

---
+++ scripts/reload-perfcounters.bat
@@ -0,0 +1,13 @@
+cd c:\Windows\Inf\W3SVC
+lodctr 0409\w3ctrs.ini
+  
+cd c:\Windows\Inf\inetinfo
+lodctr 0409\infoctrs.ini
+  
+cd c:\Windows\Inf\usbhub
+lodctr 0409\usbperf.ini
+
+cd c:\Windows\Inf\MSSQL$MICROSOFT##WID
+lodctr 0409\sqlctrWID.ini
+
+shutdown /r /t 0
